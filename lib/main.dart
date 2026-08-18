1ebecac6bb2f0d978fe106223ba201fa033a8a07
Aflah Shadil k <shadilpml@gmail.com>
2026-08-18 23:29:09 +0530

    firebase issue solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:justhospital/firebase_options.dart';
 
 import 'app/routes/app_routes.dart';
 import 'app/theme/app_theme.dart';
