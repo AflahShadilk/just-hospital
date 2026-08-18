c57b8e37beb99e45cb7ae9f5559cb37d9d035bb3
Aflah Shadil k <shadilpml@gmail.com>
2026-08-18 23:09:46 +0530

    model

--- lib/models/hospital_model.dart
+++ lib/models/hospital_model.dart
@@ -2,7 +2,7 @@ class HospitalModel {
   final String id;
   final String name;
   final String location;
-  final String image;
+  final String imageUrl;
   final double rating;
   final List<String> specialisations;
   final String address;
@@ -13,11 +13,29 @@ class HospitalModel {
     required this.id,
     required this.name,
     required this.location,
-    required this.image,
+    required this.imageUrl,
     required this.rating,
     required this.specialisations,
     required this.address,
     required this.phone,
     required this.description,
   });
+
+  factory HospitalModel.fromFirestore(
+    String documentId,
+    Map<String, dynamic> data,
+  ) {
+    return HospitalModel(
+      id: documentId,
+      name: data['name'] ?? '',
+      location: data['location'] ?? '',
+      imageUrl: data['imageUrl'] ?? '',
+      rating: (data['rating'] ?? 0).toDouble(),
+      specialisations:
+          List<String>.from(data['specialisations'] ?? []),
+      address: data['address'] ?? '',
+      phone: data['phone'] ?? '',
+      description: data['description'] ?? '',
+    );
+  }
 }
